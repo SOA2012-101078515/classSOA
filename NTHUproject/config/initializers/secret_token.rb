41be4b99ec3963a977b7ab5634eef740cc0a265f
SOA2012-101078515 <changcheng.tu@gmail.com>
2012-12-17 18:40:35 +0800

    team5

---
+++ NTHUproject/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NTHUproject::Application.config.secret_token = 'b8724ad5bac6e1d138e70a22c7f51c53eb3aa8c94fc8dd0910cb736bd77ce0cf62f4ae7d7ab8e844962f4a6d3c2e8c97d47bd5a19ad61c979109fa212ab05297'
---
+++ dashanba/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dashanba::Application.config.secret_token = '9cdc119460d9aa1c3c4a1f44650fd3097683cc9f0869c9fa1a45f9cff8818e7461a5f4a480e7a3e65e67aff2acaaecc800e959516da6a86bfa982f75492169df'

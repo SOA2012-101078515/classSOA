c0ec7a991e50591763601a269b734b70fd777589
SOA2012-101078515 <changcheng.tu@gmail.com>
2013-01-12 15:46:35 +0800

    test

--- dashanba/app/mailers/vender.rb
+++ dashanba/app/mailers/vender.rb
@@ -1,7 +1,7 @@
 class Vender < ActionMailer::Base
   default from: "dashanba.test@gmail.com"
   def confirm(vender,customer,time,people,gift,phone,email)
-        mail(:to => vender, :subject => "someone want to come!!", :body=>"customer:"+customer+"\ntime:"+time+"\npeople:"+people+"\ngift:"+gift+"\ncustomer phone:"+phone+"\nemail:"+email+"\n\n\n"+"Here are your customer records:"+"http://dashanba.herokuapp.com/venderdetail")
+        mail(:to => vender, :subject => "someone want to come!!", :body=>"customer:"+customer+"\ntime:"+time+"\npeople:"+people+"\ngift:"+gift+"\ncustomer phone:"+phone+"\nemail: "+email+"\n\n\n"+"Here are your customer records:"+"http://dashanba.herokuapp.com/venderdetail")
   end
 
   def customerconfirm(customeremail,customer,time,people,gift,phone,email)
